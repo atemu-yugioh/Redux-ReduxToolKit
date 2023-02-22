4a1b4d9035d01daf8a8c1807940a57a071efb250
Nguyen_Huu_Thieng <16521532@gm.uit.edu.vn>
2023-02-22 16:53:54 +0700

    #7 - Done CRUD with createAsyncThunk

---
+++ create-async-thunk/src/pages/blog/Blog.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import CreatePost from './components/CreatePost'
+import PostList from './components/PostList'
+
+export const Blog = () => {
+  return (
+    <div className='p-5'>
+      <CreatePost />
+      <PostList />
+    </div>
+  )
+}
---
+++ create-async-thunk/src/pages/blog/blog.slice.ts
@@ -0,0 +1,103 @@
+import { createSlice, createAsyncThunk, PayloadAction } from '@reduxjs/toolkit'
+import { Post } from 'types/blog.type'
+import http from 'utils/http'
+
+interface BlogState {
+  postList: Post[]
+  editingPost: Post | null
+}
+
+const initialState: BlogState = {
+  postList: [],
+  editingPost: null
+}
+
+export const getPostList = createAsyncThunk('blog/getPostList', async (_, thunkAPI) => {
+  const response = await http.get<Post[]>('posts', {
+    signal: thunkAPI.signal
+  })
+  return response.data
+})
+
+export const addPost = createAsyncThunk('blog/addPost', async (body: Omit<Post, 'id'>, thunkAPI) => {
+  try {
+    const response = await http.post<Post>('posts', body, {
+      signal: thunkAPI.signal
+    })
+    return response.data
+  } catch (error: any) {
+    if (error.name === 'AxiosError' && error.response.status === 422) {
+      return thunkAPI.rejectWithValue(error.response.data)
+    }
+    throw error
+  }
+})
+
+export const updatePost = createAsyncThunk(
+  'blog/updatePost',
+  async ({ postId, body }: { postId: string; body: Post }, thunkAPI) => {
+    try {
+      const response = await http.put<Post>(`posts/${postId}`, body, {
+        signal: thunkAPI.signal
+      })
+      return response.data
+    } catch (error: any) {
+      if (error.name === 'AxiosError' && error.response.status === 422) {
+        return thunkAPI.rejectWithValue(error.response.data)
+      }
+      throw error
+    }
+  }
+)
+
+export const deletePost = createAsyncThunk('blog/deletePost', async (postId: string, thunkAPI) => {
+  const response = await http.delete<Post>(`posts/${postId}`, {
+    signal: thunkAPI.signal
+  })
+
+  return response.data
+})
+
+const blogSlice = createSlice({
+  name: 'blog',
+  initialState,
+  reducers: {
+    startEditingPost: (state, action: PayloadAction<string>) => {
+      const postId = action.payload
+      const foundPost = state.postList.find((post) => post.id === postId) || null
+      state.editingPost = foundPost
+    },
+    cancelEditingPost: (state) => {
+      state.editingPost = null
+    }
+  },
+  extraReducers(builder) {
+    builder
+      .addCase(getPostList.fulfilled, (state, action) => {
+        state.postList = action.payload
+      })
+      .addCase(addPost.fulfilled, (state, action) => {
+        state.postList.push(action.payload)
+      })
+      .addCase(updatePost.fulfilled, (state, action) => {
+        const postId = action.payload.id
+        const foundIndexPost = state.postList.findIndex((post) => post.id === postId)
+        if (foundIndexPost !== -1) {
+          state.postList[foundIndexPost] = action.payload
+        }
+        state.editingPost = null
+      })
+      .addCase(deletePost.fulfilled, (state, action) => {
+        const postId = action.meta.arg
+        const foundPostIndex = state.postList.findIndex((post) => post.id === postId)
+        if (foundPostIndex !== -1) {
+          state.postList.splice(foundPostIndex, 1)
+        }
+      })
+  }
+})
+
+export const { cancelEditingPost, startEditingPost } = blogSlice.actions
+const blogReducer = blogSlice.reducer
+
+export default blogReducer
